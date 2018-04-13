0dd7df8cf026cc9620bb0d42233e532bc2df3927
theschmocker <jacobcs1996@gmail.com>
2018-04-12 22:49:54 -0500

    config additions

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,39 @@
+const { resolve, join } = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+
+const OUTPUT_PATH = './dist';
+
 module.exports = {
     module: {
         rules: [
-
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: { minimize: true },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/static', to: 'static'},
+        ]),
+        new ServiceWorkerWebpackPlugin({
+            entry: './src/sw.js',
+            excludes: ['**/.*', '**/*.map', '*.html'], 
+        }),
+    ]
 }
