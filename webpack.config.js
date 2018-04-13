9fc8a9f955a673d7315deb89843de8a42329b6f5
theschmocker <jacobcs1996@gmail.com>
2018-04-13 11:40:42 -0500

    add loaders for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const { resolve, join } = require('path');
 
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 
@@ -18,18 +18,27 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
             }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
+            template: './src/index.html',
+            filename: './index.html'
         }),
         new CopyWebpackPlugin([
             { from: 'src/static', to: 'static'},
+            { from: 'src/favicon.ico', to: 'favicon.ico'},
         ]),
         new ServiceWorkerWebpackPlugin({
             entry: './src/sw.js',
