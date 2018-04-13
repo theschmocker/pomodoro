06ffc6eaadc19255a28ea5b5ad24ea044d6275d9
theschmocker <jacobcs1996@gmail.com>
2018-04-12 22:53:51 -0500

    service worker that uses serviceworker-webpack-plugin

---
+++ src/sw.js
@@ -0,0 +1,50 @@
+// When the user navigates to your site,
+// the browser tries to redownload the script file that defined the service
+// worker in the background.
+// If there is even a byte's difference in the service worker file compared
+// to what it currently has, it considers it 'new'.
+const { assets } = global.serviceWorkerOption
+console.log(assets);
+
+let appShellFiles = [...assets, './', './static/alarm.mp3', './static/styles.css']
+
+const cacheName = "pomodorschmo-v0.1";
+    
+    //    [
+    //    '/',
+    //    '/index.html',
+    //    '/registerServiceWorker.js',
+    //    '/assets/styles.css',
+    //    '/scripts/main.js',
+    //    '/assets/alarm.mp3',
+    //];
+
+self.addEventListener('install', function(e) {
+    console.log('[Service Work] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] caching all files');
+            return cache.addAll(appShellFiles);
+        })
+    );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
