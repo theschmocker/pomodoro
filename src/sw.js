b58c7e4f390a5439d549d59ff24bd728c2cafed8
theschmocker <jacobcs1996@gmail.com>
2018-04-15 01:29:27 -0500

    remove console.logs from service worker

--- src/sw.js
+++ src/sw.js
@@ -4,17 +4,14 @@
 // If there is even a byte's difference in the service worker file compared
 // to what it currently has, it considers it 'new'.
 const { assets } = global.serviceWorkerOption
-console.log(assets);
 
 let appShellFiles = [...assets, './', './static/alarm.mp3', './static/styles.css']
 
 const cacheName = `pomodorschmo1-${new Date().toISOString()}`;
 
 self.addEventListener('install', function(e) {
-    console.log('[Service Work] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] caching all files');
             return cache.addAll(appShellFiles);
         })
     );
@@ -28,10 +25,8 @@ self.addEventListener('activate',  event => {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
         });
